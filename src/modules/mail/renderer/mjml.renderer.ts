d6228df83f086281cee4d99e1558b6d79d1141b5
Pieter <pieter.pauwels@appwise.be>
2024-02-29 14:40:57 +0100

    Use absolute path of dist dir when retrieving template file

--- src/modules/mail/renderer/mjml.renderer.ts
+++ src/modules/mail/renderer/mjml.renderer.ts
@@ -5,7 +5,7 @@ import { render, type Data } from 'ejs'
 
 @Injectable()
 export class MjmlRenderer {
-  private readonly templateDir = './src/modules/mail/templates/'
+  private readonly templateDir = process.cwd() + '/dist/src/modules/mail/templates/'
 
   public async render (templateName: string, data: Data): Promise<string> {
     const mjmlTemplateFile = await readFile(`${this.templateDir}${templateName}.mjml`, 'utf-8')
