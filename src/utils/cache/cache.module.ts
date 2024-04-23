d188baabad3b1a6d593823b823f2dfa4b908c025
JonasVannieuwenhuijsen <jonas.vannieuwenhuijsen@appwise.be>
2024-04-23 14:58:39 +0200

    cache module init fix

--- src/utils/cache/cache.module.ts
+++ src/utils/cache/cache.module.ts
@@ -2,16 +2,24 @@ import { Module } from '@nestjs/common'
 import { CacheModule } from '@nestjs/cache-manager'
 import { type RedisClientOptions } from 'redis'
 import { redisStore } from 'cache-manager-redis-store'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { RoleRepository } from '../../modules/roles/repositories/role.repository.js'
 import { UserRepository } from '../../modules/users/repositories/user.repository.js'
 import { RedisCacheService } from './cache.js'
 
 @Module({
-  imports: [CacheModule.register<RedisClientOptions>({
-    global: true,
-    store: redisStore,
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
+  imports: [CacheModule.registerAsync<RedisClientOptions>({
+    isGlobal: true,
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+      store: await redisStore({
+        socket: {
+          host: configService.get('REDIS_HOST'),
+          port: configService.get('REDIS_PORT')
+        }
+      })
+    }),
+    inject: [ConfigService]
   })],
   controllers: [],
   providers: [RedisCacheService, RoleRepository, UserRepository],
