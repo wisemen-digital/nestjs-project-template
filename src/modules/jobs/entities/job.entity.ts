3d915b72a02454de4c5216ad8b16b94d5733de38
stephen <stephen.henckaerts@appwise.be>
2024-05-15 11:50:44 +0200

    Fix

--- src/modules/jobs/entities/job.entity.ts
+++ src/modules/jobs/entities/job.entity.ts
@@ -1,5 +1,54 @@
-import { Entity } from 'typeorm'
-import { JobBase } from './job-base.entity.js'
+import { Column, Entity, PrimaryColumn } from 'typeorm'
+import { JobState } from '../enums/job-state.enum.js'
+import { type JobData } from '../types/job-data.type.js'
 
 @Entity({ name: 'job', schema: 'pgboss', synchronize: false })
-export class Job extends JobBase {}
+export class Job {
+  @PrimaryColumn('uuid', { name: 'id' })
+  uuid: string
+
+  @Column('text')
+  name: string
+
+  @Column('int')
+  priority: number
+
+  @Column('jsonb', { nullable: true })
+  data: JobData | null
+
+  @Column({
+    type: 'enum',
+    enum: JobState
+  })
+  state: JobState
+
+  @Column('int', { name: 'retrylimit' })
+  retryLimit: number
+
+  @Column('int', { name: 'retrycount' })
+  retryCount: number
+
+  @Column('int', { name: 'retrydelay' })
+  retryDelay: number
+
+  @Column('boolean', { name: 'retrybackoff' })
+  retryBackoff: boolean
+
+  @Column('timestamp', { name: 'startafter' })
+  startAfter: Date
+
+  @Column('timestamp', { name: 'startedon', nullable: true })
+  startedAt: Date | null
+
+  @Column('text', { name: 'singletonkey', nullable: true })
+  singletonKey: string | null
+
+  @Column('timestamp', { name: 'createdon' })
+  createdAt: Date
+
+  @Column('timestamp', { name: 'completedon', nullable: true })
+  completedAt: Date | null
+
+  @Column('jsonb', { nullable: true })
+  output: object | null
+}
