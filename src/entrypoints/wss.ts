2ec32deb4ed37ac374e38599f55c51cad03c607f
Joren Vandeweyer <joren.vandeweyer@appwise.be>
2024-06-10 15:03:46 +0200

    fix entrypont

--- src/entrypoints/wss.ts
+++ src/entrypoints/wss.ts
@@ -1,24 +1,16 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core'
-import { ValidationPipe } from '@nestjs/common'
-import { mainDataSource } from '../config/sql/sources/main.js'
-import { migrate } from '../config/sql/utils/typeorm.js'
 import { SocketIOModule } from '../modules/socket-io/modules/socket-io.module.js'
 import { HttpExceptionFilter } from '../utils/Exceptions/http-exception.filter.js'
 import { AppModule } from '../app.module.js'
+import { initSentry } from '../helpers/sentry.js'
 
 async function bootstrap (): Promise<void> {
-  await mainDataSource.initialize().then(async () => { await migrate(mainDataSource) })
+  initSentry()
+
   const app = await NestFactory.create(AppModule.forRoot([
     SocketIOModule.register()
   ]))
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true
-    })
-  )
   app.enableCors()
 
   const httpAdapterHost = app.get(HttpAdapterHost)
