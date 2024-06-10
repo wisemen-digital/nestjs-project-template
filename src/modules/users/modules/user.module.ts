045da43d5637bbd9fb918e6f3f1739553fbe9495
Joren Vandeweyer <joren.vandeweyer@appwise.be>
2024-06-10 12:34:40 +0200

    fix some missing things

--- src/modules/users/modules/user.module.ts
+++ src/modules/users/modules/user.module.ts
@@ -5,14 +5,11 @@ import { UserService } from '../services/user.service.js'
 import { User } from '../entities/user.entity.js'
 import { UserRepository } from '../repositories/user.repository.js'
 import { RedisCacheModule } from '../../../utils/cache/cache.module.js'
-import { PgBossModule } from '../../pgboss/pgboss.module.js'
-import { UserTypesenseJob } from '../jobs/user-typesense.job.js'
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
-    RedisCacheModule,
-    PgBossModule.forFeature([UserTypesenseJob])
+    RedisCacheModule
   ],
   controllers: [UserController],
   providers: [
