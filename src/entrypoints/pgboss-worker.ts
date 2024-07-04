64cda7e2998f7b0eb17433fd5a19e6edf12e9867
Pieter <pieter.pauwels@appwise.be>
2024-07-04 15:21:27 +0200

    Add queue argument to pgboss worker

--- src/entrypoints/pgboss-worker.ts
+++ src/entrypoints/pgboss-worker.ts
@@ -4,12 +4,23 @@ import { initSentry } from '../helpers/sentry.js'
 import { PgBossWorkerModule } from '../modules/pgboss-worker/pgboss-worker.module.js'
 import { QueueName } from '../modules/pgboss/types/queue-name.enum.js'
 
+function getArgumentValue (argName: string): string | undefined {
+  const index = process.argv.findIndex(arg => arg.startsWith(`--${argName}=`))
+  if (index === -1) return undefined
+  return process.argv[index].replace(`--${argName}=`, '')
+}
+
 async function bootstrap (): Promise<void> {
   initSentry()
 
+  const queueName = getArgumentValue('queue')
+  if (queueName == null) throw new Error('The \'queue\' argument is required')
+  if (!Object.values(QueueName).includes(queueName as QueueName)) throw new Error(`Queue ${queueName} not found`)
+  const queue = queueName as QueueName
+
   const app = await NestFactory.createApplicationContext(
     AppModule.forRoot([
-      PgBossWorkerModule.register(QueueName.TYPESENSE)
+      PgBossWorkerModule.register(queue)
     ])
   )
 
