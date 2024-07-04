369f6e5557922ff8a9f764d80741c3439477a984
Pieter <pieter.pauwels@appwise.be>
2024-07-04 16:01:19 +0200

    Make use of yargs package

--- src/entrypoints/pgboss-worker.ts
+++ src/entrypoints/pgboss-worker.ts
@@ -1,21 +1,31 @@
 import { NestFactory } from '@nestjs/core'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 import { AppModule } from '../app.module.js'
 import { initSentry } from '../helpers/sentry.js'
 import { PgBossWorkerModule } from '../modules/pgboss-worker/pgboss-worker.module.js'
 import { QueueName } from '../modules/pgboss/types/queue-name.enum.js'
 
-function getArgumentValue (argName: string): string | undefined {
-  const index = process.argv.findIndex(arg => arg.startsWith(`--${argName}=`))
-  if (index === -1) return undefined
-  return process.argv[index].replace(`--${argName}=`, '')
-}
+const argv = yargs(hideBin(process.argv))
+  .option('queue', {
+    alias: 'q',
+    type: 'string',
+    description: 'The name of the queue to handle',
+    choices: Object.values(QueueName),
+    demandOption: true
+  })
+  .help()
+  .argv
 
 async function bootstrap (): Promise<void> {
   initSentry()
 
-  const queueName = getArgumentValue('queue')
-  if (queueName == null) throw new Error('The \'queue\' argument is required')
-  if (!Object.values(QueueName).includes(queueName as QueueName)) throw new Error(`Queue ${queueName} not found`)
+  const args = await argv
+
+  const queueName = args.queue
+  if (!Object.values(QueueName).includes(queueName as QueueName)) {
+    throw new Error(`Queue ${queueName} not found`)
+  }
   const queue = queueName as QueueName
 
   const app = await NestFactory.createApplicationContext(
