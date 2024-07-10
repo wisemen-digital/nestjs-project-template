c369e950c3f5355b4c6039a178c9fcfb8d6ade00
jonasbeckers <jonas.beckers1996@gmail.com>
2024-07-10 21:54:15 +0200

    Add password grant as authentication option

--- src/entrypoints/api.ts
+++ src/entrypoints/api.ts
@@ -1,9 +1,10 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { SwaggerModule } from '@nestjs/swagger'
 import { ValidationPipe, VersioningType } from '@nestjs/common'
 import { AppModule } from '../app.module.js'
 import { initSentry } from '../helpers/sentry.js'
 import { HttpExceptionFilter } from '../utils/exceptions/http-exception.filter.js'
+import { buildDocumentationConfig } from '../helpers/documentation.js'
 
 async function bootstrap (): Promise<void> {
   const app = await NestFactory.create(
@@ -26,11 +27,7 @@ async function bootstrap (): Promise<void> {
   })
   app.enableCors()
 
-  const config = new DocumentBuilder()
-    .setTitle('API Documentation')
-    .setDescription('The API documentation description')
-    .setVersion('1.0')
-    .build()
+  const config = buildDocumentationConfig()
   const document = SwaggerModule.createDocument(app, config)
 
   SwaggerModule.setup('api/docs', app, document)
