0d14b58569cc05c37e05374a51ded942d1f61fc9
Siemen Van den Neste <48483315+siemenvdn@users.noreply.github.com>
2024-07-18 17:27:59 +0200

    use notification-service in user-flow-service

--- src/modules/users/modules/user.module.ts
+++ src/modules/users/modules/user.module.ts
@@ -8,19 +8,22 @@ import { RedisCacheModule } from '../../../utils/cache/cache.module.js'
 import { UserTypesenseRepository } from '../repositories/user-typesense.repository.js'
 import { TypesenseModule } from '../../typesense/modules/typesense.module.js'
 import { UserFlowService } from '../services/user-flow.service.js'
+import { NotificationModule } from '../../notifications/modules/notification.module.js'
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     RedisCacheModule,
-    TypesenseModule
+    TypesenseModule,
+    NotificationModule
   ],
   controllers: [UserController],
   providers: [
+    UserFlowService,
+
     UserService,
     UserRepository,
-    UserTypesenseRepository,
-    UserFlowService
+    UserTypesenseRepository
   ],
   exports: [UserService]
 })
--- src/modules/users/services/user-flow.service.ts
+++ src/modules/users/services/user-flow.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common'
-
 import { TypesenseCollectionService } from '../../typesense/services/typesense-collection.service.js'
 import { type CreateUserDto } from '../dtos/create-user.dto.js'
 import { TypesenseCollectionName } from '../../typesense/enums/typesense-collection-index.enum.js'
@@ -7,31 +6,36 @@ import { type User } from '../entities/user.entity.js'
 import { type UpdateUserDto } from '../dtos/update-user.dto.js'
 import { type UpdatePasswordDto } from '../dtos/update-password.dto.js'
 import { type UserQuery } from '../queries/user.query.js'
+import { NotificationService } from '../../notifications/services/notification.service.js'
+import { type Notification } from '../../notifications/entities/notification.entity.js'
+import { type UpdateNotificationDto } from '../../notifications/dtos/update-notification.dto.js'
 import { UserService } from './user.service.js'
 
 @Injectable()
 export class UserFlowService {
   constructor (
     private readonly userService: UserService,
+    private readonly notificationService: NotificationService,
+
     private readonly typesenseService: TypesenseCollectionService
   ) {}
 
-  async findPaginatedAndCount (query: UserQuery): Promise<[User[], number]> {
+  public async findPaginatedAndCount (query: UserQuery): Promise<[User[], number]> {
     return await this.userService.findPaginated(query)
   }
 
-  async findOneOrFail (userUuid: string): Promise<User> {
+  public async findOneOrFail (userUuid: string): Promise<User> {
     return await this.userService.findOneOrFail(userUuid)
   }
 
-  async create (dto: CreateUserDto): Promise<User> {
+  public async create (dto: CreateUserDto): Promise<User> {
     const user = await this.userService.create(dto)
     await this.typesenseService.importManuallyToTypesense(TypesenseCollectionName.USER, [user])
 
     return user
   }
 
-  async update (userUuid: string, dto: UpdateUserDto): Promise<User> {
+  public async update (userUuid: string, dto: UpdateUserDto): Promise<User> {
     const updatedUser = await this.userService.update(userUuid, dto)
     await this.typesenseService.importManuallyToTypesense(
       TypesenseCollectionName.USER,
@@ -40,12 +44,24 @@ export class UserFlowService {
     return updatedUser
   }
 
-  async updatePassword (userUuid: string, dto: UpdatePasswordDto): Promise<User> {
+  public async updatePassword (userUuid: string, dto: UpdatePasswordDto): Promise<User> {
     return await this.userService.updatePassword(userUuid, dto)
   }
 
-  async delete (userUuid: string): Promise<void> {
+  public async delete (userUuid: string): Promise<void> {
     await this.userService.delete(userUuid)
     await this.typesenseService.deleteFromTypesense(TypesenseCollectionName.USER, userUuid)
+  }
+
+  public async findNotificationSettings (
+    userUuid: string, deviceUuid: string
+  ): Promise<Notification> {
+    return await this.notificationService.findOrCreateNotification(userUuid, deviceUuid)
+  }
+
+  public async updateNotificationSettings (
+    userUuid: string, deviceUuid: string, dto: UpdateNotificationDto
+  ): Promise<Notification> {
+    return await this.notificationService.updateNotification(userUuid, deviceUuid, dto)
   }
 }
