62e4f17c2f3f7a8050928f0651cf067dfe813aa6
JonasVannieuwenhuijsen <jonas.vannieuwenhuijsen@appwise.be>
2024-07-22 10:55:22 +0200

    add yargs

--- src/entrypoints/cronjob.ts
+++ src/entrypoints/cronjob.ts
@@ -1,9 +1,26 @@
-import { CronjobFactory } from './cronjobs/factories/cronjob.factory.js'
+import yargs from 'yargs'
+import { CronjobFactory, CronJobType } from './cronjobs/factories/cronjob.factory.js'
 
-async function bootstrap (jobName: string): Promise<void> {
+async function bootstrap (): Promise<void> {
+  const args = await yargs(process.argv)
+    .option('job', {
+      alias: 'j',
+      type: 'string',
+      description: 'The name of the cronjob to run',
+      choices: Object.values(CronJobType),
+      demandOption: true
+    })
+    .help()
+    .argv
+
+  const jobName = args.job
+  if (!Object.values(CronJobType).includes(jobName as CronJobType)) {
+    throw new Error(`Job ${jobName} not found`)
+  }
+
   const cronjob = CronjobFactory.create(jobName)
 
   await cronjob.run()
 }
 
-await bootstrap(process.argv[2])
+await bootstrap()
