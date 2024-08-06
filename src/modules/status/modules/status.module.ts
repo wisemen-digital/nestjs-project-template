3ac9b9667261703c00251b93dc35571cdf8432de
sebastiaanvanspauwen <sebastiaan.vanspauwen@appwise.be>
2024-08-06 21:01:55 +0200

    Added first draft of liveness and readiness probes.

--- src/modules/status/modules/status.module.ts
+++ src/modules/status/modules/status.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common'
+import { TerminusModule } from '@nestjs/terminus'
 import { StatusController } from '../controllers/status.controller.js'
+import { RedisClient } from '../../redis/redis.client.js'
+import { RedisHealthIndicator } from '../health/redis.health.js'
+import { TypesenseClient } from '../../typesense/clients/typesense.client.js'
+import { TypesenseHealthIndicator } from '../health/typesense.health.js'
+import { AppStateService } from '../services/app-state.service.js'
 
 @Module({
-  imports: [],
+  imports: [TerminusModule],
   controllers: [StatusController],
-  providers: [],
+  providers: [
+    RedisClient,
+    RedisHealthIndicator,
+    TypesenseClient,
+    TypesenseHealthIndicator,
+    AppStateService
+  ],
   exports: []
 })
 export class StatusModule {}
