e31f29f98317e387662499c760a5c912153f32f6
Kobe Kwanten <kobe.kwanten@appwise.be>
2024-08-29 16:14:58 +0200

    Fix import orders

--- src/utils/exceptions/api-errors/bad-request.api-error.ts
+++ src/utils/exceptions/api-errors/bad-request.api-error.ts
@@ -1,6 +1,6 @@
+import { HttpStatus } from '@nestjs/common'
 import { ApiError } from './api-error.js'
 import { ApiErrorStatus } from './api-error-status.decorator.js'
-import { HttpStatus } from '@nestjs/common'
 
 export abstract class BadRequestApiError extends ApiError {
   @ApiErrorStatus(HttpStatus.BAD_REQUEST)
